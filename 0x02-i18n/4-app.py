1fb3c1cf425f1a8e3638542fec5861f88a3ea709
Uchechukwu Ottah <usottah@gmail.com>
2023-04-25 15:43:42 +0000

    Update: pass locale parameter ny force

---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+""" Module definition """
+
+from flask import Flask, render_template, request
+from flask_babel import Babel, _
+from pytz import UTC
+
+
+class Config(object):
+    """ Config class foe flask_babel instance """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """ gets locale best match nased off config """
+    locales = app.config['LANGUAGES']
+    locale = request.args.get('locale')
+
+    if locale in locales:
+        return locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """ index route """
+    return render_template('4-index.html')
+
+
+if __name__ == "__main__":
+    app.run(port="5000", host="0.0.0.0", debug=True)
