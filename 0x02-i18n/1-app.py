27dd8dc6d7d01691e35856c5d6962b701c38e7cd
Uchechukwu Ottah <usottah@gmail.com>
2023-04-25 12:36:14 +0000

    Update: Task 1

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -2,15 +2,22 @@
 """ Module definition """
 
 from flask import Flask, render_template
+from flask_babel import Babel
+from pytz import UTC
 
 
 app = Flask(__name__)
+babel = Babel(app)
 
+class Config(object):
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = UTC
 
 @app.route('/')
 def index():
     """ index route """
-    return render_template('0-index.html')
+    return render_template('1-index.html')
 
 
 if __name__ == "__main__":
