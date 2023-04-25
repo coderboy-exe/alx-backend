3aa36f38116e760a6959da0c5609cfb0220c6630
Uchechukwu Ottah <usottah@gmail.com>
2023-04-25 12:54:33 +0000

    Update: flask_babel config

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 """ Module definition """
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_babel import Babel
 from pytz import UTC
 
 
 class Config(object):
+    """ Config class foe flask_babel instance """
     LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = UTC
@@ -18,10 +19,16 @@ app.config.from_object(Config)
 babel = Babel(app)
 
 
+@babel.localeselector
+def get_locale():
+    """ gets locale best match nased off config """
+    return request.accepted_languages.best_match(app.config['LANGUAGES'])
+
+
 @app.route('/')
 def index():
     """ index route """
-    return render_template('1-index.html')
+    return render_template('2-index.html')
 
 
 if __name__ == "__main__":
