3b40f609aec1cab5a463978342a4006dee478bb8
Uchechukwu Ottah <usottah@gmail.com>
2023-04-25 13:19:31 +0000

    Update

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -10,7 +10,7 @@ class Config(object):
     """ Config class foe flask_babel instance """
     LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LOCALE = 'en'
-    BABEL_DEFAULT_TIMEZONE = UTC
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
 app = Flask(__name__)
