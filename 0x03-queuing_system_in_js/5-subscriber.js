16159d9df6a534813b1ea5a650ab20889a85cdf6
Uchechukwu Ottah <usottah@gmail.com>
2023-05-24 14:12:01 +0000

    Redis-client event subscriber and Listener

---
+++ 0x03-queuing_system_in_js/5-subscriber.js
@@ -0,0 +1,16 @@
+import { createClient } from 'redis';
+
+const client = createClient();
+
+client.on('error', err => console.log('Redis client not connected to the server:', err));
+client.on('connect', connect => console.log('Redis client connected to the server'));
+
+client.subscribe('holberton school channel');
+
+client.on('message', (channel, message) => {
+	if (message === 'KILL_SERVER') {
+	  client.unsubscribe('holberton school channel');
+	  client.end(true)
+	}
+	return console.log(message);
+});
